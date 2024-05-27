e4906ffc02b3e1db2c11ea721347b08d3e5a2206
Josue Garces Jouvin <josuejouvin27@gmail.com>
2024-05-26 23:40:07 -0500

    show order

---
+++ src/hooks/useOrder.ts
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import { MenuItem, OrderItem } from "../types"
+
+
+export default function useOrder() {
+    const [order, setOrder] = useState<OrderItem[]>([])
+
+    const addItem = (item: MenuItem) => {
+        const itemExist = order.find(orderItem => orderItem.id === item.id)
+
+        if (itemExist) {
+            const updateOrder = order.map(orderItem => orderItem.id === item.id ?
+                { ...orderItem, quantity: orderItem.quantity + 1 } :
+                orderItem
+            )
+            setOrder(updateOrder)
+        } else {
+            const newItem = { ...item, quantity: 1 }
+            setOrder([...order, newItem])
+        }
+
+    }
+
+    const removeItem = (id : MenuItem["id"]) => {
+        setOrder(order.filter(item => item.id !== id))
+    }
+
+
+    return {
+        order,
+        addItem,
+        removeItem
+    }
+}
